ed90bfaaf6a57421f97e590b8f7d8b2aea7d2a97
ielammari <271756717+ielammari@users.noreply.github.com>
2026-05-18 04:47:48 +0100

    Red player shows up in Spawn and is animated and movable. Elements added.

--- iccoop/src/main/java/ch/epfl/cs107/icoop/ICoop.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/ICoop.java
@@ -1,23 +1,38 @@
 package ch.epfl.cs107.icoop;
 
+import ch.epfl.cs107.icoop.actor.Element;
+import ch.epfl.cs107.icoop.actor.ICoopPlayer;
+import ch.epfl.cs107.icoop.area.ICoopArea;
 import ch.epfl.cs107.icoop.area.OrbWay;
 import ch.epfl.cs107.icoop.area.Spawn;
 import ch.epfl.cs107.play.areagame.AreaGame;
 import ch.epfl.cs107.play.io.FileSystem;
+import ch.epfl.cs107.play.math.Orientation;
 import ch.epfl.cs107.play.window.Window;
 
+import static ch.epfl.cs107.icoop.KeyBindings.RED_PLAYER_KEY_BINDINGS;
+
 public class ICoop extends AreaGame {
+
+    private ICoopPlayer redPlayer;
 
     private void createAreas() {
         addArea(new Spawn());
         addArea(new OrbWay());
     }
 
+    private void initArea(String title) {
+        ICoopArea area = (ICoopArea) setCurrentArea(title, true);
+        redPlayer = new ICoopPlayer(area, Orientation.DOWN, area.getRedPlayerSpawnPosition(),
+                RED_PLAYER_KEY_BINDINGS, Element.FIRE);
+        redPlayer.enterArea(area, area.getRedPlayerSpawnPosition());
+    }
+
     @Override
     public boolean begin(Window window, FileSystem fileSystem) {
         if (super.begin(window, fileSystem)) {
             createAreas();
-            setCurrentArea("Spawn", true);
+            initArea("Spawn");
             return true;
         }
         return false;
--- iccoop/src/main/java/ch/epfl/cs107/icoop/actor/ICoopPlayer.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/ICoopPlayer.java
@@ -1,9 +1,108 @@
 package ch.epfl.cs107.icoop.actor;
 
+import ch.epfl.cs107.icoop.KeyBindings;
+import ch.epfl.cs107.play.areagame.actor.MovableAreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.engine.actor.OrientedAnimation;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.window.Button;
+import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Keyboard;
+
+import java.util.Collections;
+import java.util.List;
+
 /**
  * A ICoopPlayer is a player for the ICoop game.
  */
-public class ICoopPlayer  {
+public class ICoopPlayer extends MovableAreaEntity implements ElementalEntity{
 
-  // TO BE COMPLETED
+    private static final int ANIMATION_DURATION = 4;
+
+    private final KeyBindings.PlayerKeyBindings keys;
+    private final Element element;
+    private final OrientedAnimation animation;
+
+    public ICoopPlayer(Area owner, Orientation orientation, DiscreteCoordinates coordinates,
+                       KeyBindings.PlayerKeyBindings keys, Element element) {
+        super(owner, orientation, coordinates);
+        this.keys = keys;
+        this.element = element;
+        String spriteName = (element == Element.FIRE) ? "icoop/player" : "icoop/player2";
+        this.animation = new OrientedAnimation(spriteName, ANIMATION_DURATION, this, Vector.ZERO,
+                new Orientation[]{Orientation.DOWN, Orientation.RIGHT, Orientation.UP, Orientation.LEFT},
+                4, 1, 2, 16, 32, true);
+        resetMotion();
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        Keyboard keyboard = getOwnerArea().getKeyboard();
+        moveIfPressed(Orientation.UP, keyboard.get(keys.up()));
+        moveIfPressed(Orientation.LEFT, keyboard.get(keys.left()));
+        moveIfPressed(Orientation.DOWN, keyboard.get(keys.down()));
+        moveIfPressed(Orientation.RIGHT, keyboard.get(keys.right()));
+        if (isDisplacementOccurs()) {
+            animation.update(deltaTime);
+        } else {
+            animation.reset();
+        }
+        super.update(deltaTime);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        animation.draw(canvas);
+    }
+
+    private void moveIfPressed(Orientation orientation, Button button) {
+        if (button.isDown() && !isDisplacementOccurs()) {
+            orientate(orientation);
+            move(ANIMATION_DURATION);
+        }
+    }
+
+    public void enterArea(Area area, DiscreteCoordinates position) {
+        area.registerActor(this);
+        area.setViewCandidate(this);
+        setOwnerArea(area);
+        setCurrentPosition(position.toVector());
+        resetMotion();
+    }
+
+    public void leaveArea() {
+        getOwnerArea().unregisterActor(this);
+    }
+
+    @Override
+    public Element element() {
+        return element;
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return true;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return true;
+    }
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+    }
 }
